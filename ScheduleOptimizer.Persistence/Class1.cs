e1a4e5875faa414319b6a0fb16a712ad40345945
omner.allen <scholarshipseeker2021@gmail.com>
2024-11-04 17:55:53 -0700

    Added Professor Class and updated Persistence.

--- ScheduleOptimizer.Persistence/Class1.cs
+++ ScheduleOptimizer.Persistence/Class1.cs
@@ -2,5 +2,10 @@
 
 public class Class1
 {
+    public List<Logic.Professor> Professors { get; set; } = new List<Logic.Professor>();
+    
+    // remember to make constructor in professor class to initialize a ton of professors for this. 
+
+    
 
 }
