a553eb8b87e42aa8c0e384ce47cc5162ba8c4f8d
omner.allen <scholarshipseeker2021@gmail.com>
2024-11-06 14:16:48 -0700

    Partially set up Professor Initializer

--- ScheduleOptimizer.Logic/Professor.cs
+++ ScheduleOptimizer.Logic/Professor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,20 @@ namespace ScheduleOptimizer.Logic
 {
     public class Professor
     {
+        private List<(Course, Enum)> listofPreference;
+        public Professor(List<(Course,Enum)> courseList)
+        {
+            listofPreference = courseList; 
+
+        }
+        
+    public static List<(Course,Professor)> CalculatePreferences()
+        {
+            // Calculate professor preference and return a list of course and the optimum professor
+            return new List<(Course, Professor)> ();
+        }
+
+
 
     }
 }
---
+++ ScheduleOptimizer.Persistence/Persistence.cs
@@ -0,0 +1,17 @@
+﻿namespace ScheduleOptimizer.Persistence;
+
+public class Persistence
+{
+    public List<Logic.Professor> Professors { get; set; } = new List<Logic.Professor>();
+    
+    // remember to make constructor in professor class to initialize a ton of professors for this. 
+
+    private enum Preference
+    {
+        pleaseNo,   // 0
+        NotFavored, // 1
+        Neutral,    // 2
+        Favored,    // 3
+    }
+
+}
