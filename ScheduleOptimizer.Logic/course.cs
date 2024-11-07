56155469c45f71b69dd6db8cc20959dcdcc26966
KadenBaxter <79764920+kadebaxter@users.noreply.github.com>
2024-11-07 01:47:36 -0700

    Solved all merge conflicts and errors (#5)
    * Added initialize data class, cleared all errors and merge conflicts.

--- ScheduleOptimizer.Logic/course.cs
+++ ScheduleOptimizer.Logic/course.cs
@@ -5,23 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ScheduleOptimizer.Logic
-public class Course
 {
-    public int CourseId { get; set; }
-    public string CourseName { get; set; }
-    public string Description { get; set; }
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Description { get; set; }
 
-    public Course(int courseId, string courseName, string description)
-    {
-        CourseId = courseId;
-        CourseName = courseName;
-        Description = description;
-    }
-n
-    public void DisplayCourseInfo()
-    {
-        Console.WriteLine($"Course ID: {CourseId}");
-        Console.WriteLine($"Course Name: {CourseName}");
-        Console.WriteLine($"Description: {Description}");
+        public Course(int courseId, string courseName)
+        {
+            CourseId = courseId;
+            CourseName = courseName;
+            Description = "N/A";
+        }
+        public Course(int courseId, string courseName, string description)
+        {
+            CourseId = courseId;
+            CourseName = courseName;
+            Description = description;
+        }
+
+        public void DisplayCourseInfo()
+        {
+            Console.WriteLine($"Course ID: {CourseId}");
+            Console.WriteLine($"Course Name: {CourseName}");
+            Console.WriteLine($"Description: {Description}");
+        }
     }
 }
--- ScheduleOptimizer.Tests/UnitTest1.cs
+++ ScheduleOptimizer.Tests/UnitTest1.cs
@@ -8,15 +8,6 @@ public class UnitTest1
     [Fact]
     public void ProfessorPreference()
     {
-        Class1 newclass = new Class1();
-        int y = 2;
-        Assert.NotEqual(y, newclass.x);
-    }
-
-    public void Test2()
-    {
-        Scheduling class2 = new Scheduling();
-        int x = 4;
-        Assert.NotEqual(x, class2.y);
+       
     }
 }
