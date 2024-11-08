28c31706ab86b2faa5ef1ece909fd0f0521d7850
KadenBaxter <kadebaxter@gmail.com>
2024-11-08 11:50:39 -0700

    Clarified static method and test set up

--- ScheduleOptimizer.Tests/UnitTest1.cs
+++ ScheduleOptimizer.Tests/UnitTest1.cs
@@ -5,6 +5,10 @@ namespace ScheduleOptimizer.Tests;
 
 public class UnitTest1
 {
+    //Before making a test make sure to initialize the data in the following way
+    //InitializeData.BeginData();
+    //Then clear after the test is over
+    //InitializeData.ClearData()
     [Fact]
     public void ProfessorPreference()
     {
