acec9978dd1e606dd98064fecec12a8a1aa09346
KadenBaxter <kadebaxter@gmail.com>
2024-11-13 13:48:59 -0700

    Added the CourseRoom class

---
+++ ScheduleOptimizer.Logic/CourseRooms.cs
@@ -0,0 +1,12 @@
+using ScheduleOptimizer.Logic;
+
+public class CourseRoom
+{
+    public Course Course { get; set; }
+    public List<Room> Rooms { get; set; }
+    public CourseRoom(Course course, List<Room> rooms)
+    {
+        Course = course;
+        Rooms = rooms;
+    }
+}
