a6a6a316ea88a6f99ada21b9bcf547b78cc1ea2f
Edmund Kwakye <tallboyeddy@gmail.com>
2024-11-18 12:06:41 -0800

    Razor.razor new

---
+++ courserrom.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using ScheduleOptimizer.Logic;
+
+public class CourseRoom
+{
+    public Course Course { get; set; }
+    public List<Room> Rooms { get; set; }
+    public CourseRoom(Course course, List<Room> rooms)
+    {
+        Course = course;
+        Rooms = rooms;
+
+    }
+
+   
+}  
