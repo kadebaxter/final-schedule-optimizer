527a338977375516689dd354a10ab4abd4baf32a
omner.allen <scholarshipseeker2021@gmail.com>
2024-11-20 14:15:26 -0700

    AddProfessor and Remove Professor UI completed

--- ScheduleOptimizer.WebUI/Program.cs
+++ ScheduleOptimizer.WebUI/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+//builder.Services.AddSingleton<myContextdataHolder>();// In here you can initialize your InitializeData class. 
 
 var app = builder.Build();
 
